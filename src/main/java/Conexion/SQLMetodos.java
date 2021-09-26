55e7bd7c964a5a07cac4d10990e8cda65ee23b45
wilimartinez <90808126+wilimartinez@users.noreply.github.com>
2021-09-26 14:50:05 -0500

    REGISTRAR ADMINISTRADORT1

--- src/main/java/Conexion/SQLMetodos.java
+++ src/main/java/Conexion/SQLMetodos.java
@@ -33,45 +33,5 @@ public class SQLMetodos {
             lista1.add(pol);
         }
         return lista1;
-    }
-    
-    public ArrayList<String> mnacionalidades() throws SQLException{
-        ArrayList<String> lista2 = new ArrayList<String>();
-        String sql = "SELECT * FROM nacionalidad";
-        ResultSet rs = conexion.query(sql);
-        while(rs.next()){
-            lista2.add(rs.getString("nac_nacionalidad"));
-        }
-        return lista2;
-    }
-    
-    public ArrayList<String> msexo() throws SQLException{
-        ArrayList<String> lista3 = new ArrayList<String>();
-        String sql = "SELECT * FROM sexo";
-        ResultSet rs = conexion.query(sql);
-        while(rs.next()){
-            lista3.add(rs.getString("sex_sexo"));
-        }
-        return lista3;
-    }
-    
-    public ArrayList<String> mformacion() throws SQLException{
-        ArrayList<String> lista4 = new ArrayList<String>();
-        String sql = "SELECT * FROM formacion";
-        ResultSet rs = conexion.query(sql);
-        while(rs.next()){
-            lista4.add(rs.getString("for_formacion"));
-        }
-        return lista4;
-    }
-    
-    public ArrayList<String> mdireccion() throws SQLException{
-        ArrayList<String> lista5 = new ArrayList<String>();
-        String sql = "SELECT * FROM direccion";
-        ResultSet rs = conexion.query(sql);
-        while(rs.next()){
-            lista5.add(rs.getString("dir_direccion"));
-        }
-        return lista5;
     }*/
 }
